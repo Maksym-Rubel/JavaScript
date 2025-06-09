c2833b0e88d462930bfe7718a3f5804649066507
Maks <rubelmaksum2404@gmail.com>
2025-06-09 20:37:55 +0300

    math

--- 11_class/js/index.js
+++ 11_class/js/index.js
@@ -1,25 +1,33 @@
-const Url = "https://api.github.com/users/Maksym-Rubel";
+
+let Url = "https://api.github.com/users/";
+function updateName() {
+    const input = document.getElementById("nameInput").value;
+    Url += input
+    console.log(Url)
+    searchProduct()
+
+}
+
+
 
 async function searchProduct() {
     const res = await fetch(Url);
     const data = await res.json();
-if(data != null)
-{
-    document.getElementsByClassName("profile-img")[0].src = data.avatar_url;
-    document.getElementById("userName").textContent = data.login
-    document.getElementById("userName1").textContent = data.login
-    document.getElementById("urlprof").innerHTML = `<a href="${data.html_url}">${data.html_url}</a>`
-    document.getElementById("Email").textContent = data.email != null ? data.email : "None Email"
-    document.getElementById("followers").textContent = data.followers
-    document.getElementById("following").textContent = data.following
-}
-else
-{
-    alert("Not Found 404")
-}
-    
+    if (data != null) {
+        document.getElementsByClassName("profile-img")[0].src = data.avatar_url;
+        document.getElementById("userName").textContent = data.login
+        document.getElementById("userName1").textContent = data.login
+        document.getElementById("urlprof").innerHTML = `<a href="${data.html_url}">${data.html_url}</a>`
+        document.getElementById("Email").textContent = data.email != null ? data.email : "None Email"
+        document.getElementById("followers").textContent = data.followers
+        document.getElementById("following").textContent = data.following
+    }
+    else {
+        alert("Not Found 404")
+    }
+
 
 }
 
 
-searchProduct()
+
