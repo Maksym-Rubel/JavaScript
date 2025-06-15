f82d703bda038f2cb824006c921cf9cd49cfaef8
Maks <rubelmaksum2404@gmail.com>
2025-06-15 12:11:15 +0300

    math

--- 09_DOM/js/index.js
+++ 09_DOM/js/index.js
@@ -1,35 +1,42 @@
-// let newWindow = window.open('', '', 'width=410,height=410');
+let newWindow = window.open('', '', 'width=410,height=410');
 
-// setTimeout(function(){
-//     if(newWindow)
-//     {
-//         newWindow.resizeTo(500,500)
-//     }
-// },2000)
+setTimeout(function(){
+    if(newWindow)
+    {
+        newWindow.resizeTo(500,500)
+    }
+},2000)
 
-// setTimeout(function(){
-//     if(newWindow)
-//     {
-//         newWindow.moveTo(200,200)
-//     }
-// },4000)
+setTimeout(function(){
+    if(newWindow)
+    {
+        newWindow.moveTo(200,200)
+    }
+},4000)
 
-// setTimeout(function(){
-//     if(newWindow)
-//     {
-//         newWindow.close()
-//     }
-// },6000)
+setTimeout(function(){
+    if(newWindow)
+    {
+        newWindow.close()
+    }
+},6000)
 
 
 ////////////////////////////////////////////////////////////
-
-function updateTime() {
+function SelectSec(){
     const now = new Date();
-    const hours = String(now.getHours())
-    const minutes = String(now.getMinutes())
+    const seconds = String(now.getSeconds());
 
-    document.getElementById('time').textContent = `${hours < 10 ? "0" + hours : hours}:${minutes < 10 ? "0" + minutes : minutes}`;
+
+    return seconds * 6;
+}
+angle = 360 / 60;
+angleadd = SelectSec();
+const clock = document.getElementById('simg1');
+function updateTime() {
+    
+    clock.style.transform = `rotate(${angleadd += angle}deg)`;
+    
 }
 
 setInterval(updateTime, 1000);
