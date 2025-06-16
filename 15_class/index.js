f29abf5417730485addfb8c8539bd591b95f42ce
Maks <rubelmaksum2404@gmail.com>
2025-06-16 20:48:59 +0300

    math

--- 15_class/index.js
+++ 15_class/index.js
@@ -1,39 +1,50 @@
-const $addBtn = $('#addBtn');
-const $container = $('#container');
-$addBtn.on('click', function () {
+// const $addBtn = $('#addBtn');
+// const $container = $('#container');
+// $addBtn.on('click', function () {
 
-    const $block = $('<div class="block"></div>');
-    $block.css('background-color', getRandomColor());
-    $('#container').append($block);
-});
-
-
-
-function getRandomColor() {
-    const r = Math.floor(Math.random() * 256);
-    const g = Math.floor(Math.random() * 256);
-    const b = Math.floor(Math.random() * 256);
-    return `rgb(${r}, ${g}, ${b})`;
-}
-
-
-$container.on('click', `.block`, function () {
-    $(this).remove();
-});
+//     const $block = $('<div class="block"></div>');
+//     $block.css('background-color', getRandomColor());
+//     $('#container').append($block);
+// });
 
 
 
-const $circleses = $('.circleses');
-const $btn1 = $('#btn1');
-let last = 0;
-let count = 0;
-$btn1.on('click', function () {
-    $circleses.children().eq(last).css('backgroundColor', 'gainsboro');
-    $circleses.children().eq(count).css('backgroundColor', count == 0 ? 'red' : count == 1 ? 'blue' : 'green');
-    last = count;
-    count++;
-    if (count >= 3) {
-        count = 0;
+// function getRandomColor() {
+//     const r = Math.floor(Math.random() * 256);
+//     const g = Math.floor(Math.random() * 256);
+//     const b = Math.floor(Math.random() * 256);
+//     return `rgb(${r}, ${g}, ${b})`;
+// }
+
+
+// $container.on('click', `.block`, function () {
+//     $(this).remove();
+// });
+
+
+
+// const $circleses = $('.circleses');
+// const $btn1 = $('#btn1');
+// let last = 0;
+// let count = 0;
+// $btn1.on('click', function () {
+//     $circleses.children().eq(last).css('backgroundColor', 'gainsboro');
+//     $circleses.children().eq(count).css('backgroundColor', count == 0 ? 'red' : count == 1 ? 'blue' : 'green');
+//     last = count;
+//     count++;
+//     if (count >= 3) {
+//         count = 0;
+//     }
+// });
+
+const $container = $('.container');
+
+
+$container.on('click', 'div', function () {
+    for (let i = 1; i <= $(this).index(); i++) {
+        $container.children().eq(i).css('color', 'red');
+    }
+    for (let i = $(this).index() + 1; i <= $container.children().length; i++) {
+        $container.children().eq(i).css('color', 'black');
     }
 });
-
